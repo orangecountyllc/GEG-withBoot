15e1ff3eb76e62fd68ca95f1271310eaecb91bae
Jay <jpca999@gmail.com>
2024-03-17 16:01:46 -0400

    now staring to inject nav into the about page

--- public/New/responsive-sticky-navbar-master/sticky.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).on('scroll',function(){
-        if($(window).scrollTop()){
-            $(".sub-header").addClass('active');
-        }else{
-            $(".sub-header").removeClass('active');
-        }
-    });
-});
